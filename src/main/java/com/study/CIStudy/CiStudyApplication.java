9133dfc190576d3b32f7cf6ef240f31a4858b023
QALair <93294785+QALair@users.noreply.github.com>
2022-07-05 16:16:39 -0300

    Initial commit

---
+++ src/main/java/com/study/CIStudy/CiStudyApplication.java
@@ -0,0 +1,13 @@
+package com.study.CIStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiStudyApplication.class, args);
+	}
+
+}
